a7ef1bba2075e7fd251917cdcbf8fa00fe16397e
Muhammad Wahyu Ramadhan <mwahyuap2018@gmail.com>
2024-11-20 08:17:22 +0700

    layout: creating new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ThreeDScene from "../components/ThreeDScene";
 export default function Home() {
   return (
     <main className="p-0 m-0 h-full">
-      <ThreeDScene />
+      <h1>Hallo Dunia</h1>
     </main>
   );
 }
--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>Testing</div>
-  )
-}
-
-export default page
